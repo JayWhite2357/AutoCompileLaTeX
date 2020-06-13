f926ccbe8f8e12f686b396e3ea2a03d643ba8957
Jay White <JayWhite2357@users.noreply.github.com>
2020-06-13 14:30:16 -0400

    cache macaulay2 as well

--- 00_addM2Repo.sh
+++ 00_addM2Repo.sh
@@ -1,4 +1,3 @@
 echo "deb https://faculty.math.illinois.edu/Macaulay2/Repositories/Ubuntu $(lsb_release -c | cut -f2) main" | sudo tee /etc/apt/sources.list.d/macaulay2.list
 sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-key CD9C0E09B0C780943A1AD85553F8BD99F40DCB31
 sudo apt update -q
-sudo apt-get install -y -q macaulay2
